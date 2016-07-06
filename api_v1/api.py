058547a8852d931f9bfaf13f8005e7f08e5fbf0d
Alex Kiura <alex.kiura@andela.com>
2016-07-06 16:51:04 +0300

    [Feature] Add FB auth & rename app

--- api_v1/api.py
+++ api_v1/api.py
@@ -1,3 +1,33 @@
+"""Endpoints to allow for user creation, image upload & filtering."""
 from django.contrib.auth.models import User
-from rest_framework import viewsets
-from rest_framework.decorators import detail_route, list_route
+from rest_framework import generics
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from serializers import UserSerializer
+
+
+class UserCreateView(generics.CreateAPIView):
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
+
+    def create(self, request):
+        data = request.data
+        username, email = data.get('username'), data.get('email')
+        password, confirm_password = data.get('password'),\
+            data.get('confirm_password')
+        if password == confirm_password:
+            serializer = self.get_serializer(data=data)
+            if serializer.is_valid():
+                User.objects.create_user(username=username, password=password,
+                                         email=email)
+                return Response(serializer.data,
+                                status=status.HTTP_201_CREATED)
+            else:
+                return Response({'error':
+                                 'The email was not valid.'},
+                                status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({'error': 'The passwords do not match'},
+                            status=status.HTTP_400_BAD_REQUEST)
