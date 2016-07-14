b370d2bf8e6510b5ef2cdb6c9528cc59e6fcec45
Alex Kiura <alex.kiura@andela.com>
2016-07-14 05:47:02 +0300

    [Chore 126157345] Add thumbnail urls.

--- api_v1/urls.py
+++ api_v1/urls.py
@@ -17,13 +17,12 @@ Including another URLconf
 from django.conf.urls import url, include
 from rest_framework_jwt.views import obtain_jwt_token
 from rest_framework_jwt.views import refresh_jwt_token
-from api import UserCreateView, ImageView
-# from rest_framework.routers import DefaultRouter
+from api import UserCreateView, ImageView, ThumbnailView
 from rest_framework_nested import routers
 
 router = routers.SimpleRouter()
 router.register(r'images', ImageView)
-router.register(r'filters', ImageView)
+router.register(r'thumbnails', ThumbnailView)
 
 
 urlpatterns = [
