68f1b552d21c4bdbc8984094a5ad9eca62843423
Alex Kiura <alex.kiura@andela.com>
2016-07-22 04:55:54 +0300

    [Feature 126837405] Add helper methods for getting & presenting images.

--- static/js/components/home.jsx
+++ static/js/components/home.jsx
@@ -2,14 +2,69 @@ import React from 'react';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Menu from './menu.jsx';
+import SideBar from './sideBar.jsx';
+import request from 'superagent';
+
+const generateFolders = (imageList) => (
+  // return a list of unique folders
+  Array.from(new Set(imageList.map((image) => {
+    if (image.folder_name === '') {
+      return 'untitled';
+    }
+    return image.folder_name;
+  })))
+);
+
+const organizeImages = (imageList, folderList) => {
+  return folderList.map((folder) => (
+    {[folder]: imageList.filter((image) => {
+      if (image.folder_name === folder) {
+        return image
+      } else if(image.folder_name === '' && folder === 'untitled') {
+        return image
+      }
+    })}
+  ))
+}
+
+const fetchImage = () => {
+  // returns a Promise object.
+  return new Promise((resolve, reject) => {
+    request
+    .get('/api/v1/images/')
+    .set('Authorization', `Bearer facebook ${localStorage.getItem('accessToken')}`)
+    .end((error, result) => {
+      if (!error) {
+        resolve(result.body);
+      } else {
+        reject(error);
+      }
+    });
+  });
+};
+
 
 const Home = () => {
+  let images = []
   if (localStorage.getItem('accessToken')) {
-    return (<MuiThemeProvider muiTheme={getMuiTheme()}>
-    <div>
-      <Menu />
-      Welcome to pixlr
-    </div>
+    fetchImage().then((response) => {
+      images = response;
+      console.log(organizeImages(response, generateFolders(response)));
+    });
+    return (
+    <MuiThemeProvider muiTheme={getMuiTheme()}>
+      {/*<div className="row start-xs">
+        <Menu />
+        Welcome to pixlr
+      </div>*/}
+      <div>
+        <Menu />
+        <div className="row start-xs">
+          <div className="col-xs-3">
+            <SideBar images={images}/>
+          </div>
+        </div>
+      </div>
   </MuiThemeProvider>)
 } else {
   window.location.href = '/';
