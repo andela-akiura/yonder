4fb51d0aed4e64130205a5e55628578e2c403d5b
Alex Kiura <alex.kiura@andela.com>
2016-07-26 06:35:03 +0300

    [Feature 126484659] Add method to update canvas on click.

--- static/js/components/sideBar.jsx
+++ static/js/components/sideBar.jsx
@@ -1,52 +1,51 @@
 import React from 'react';
-import {List, ListItem} from 'material-ui/List';
-import ActionGrade from 'material-ui/svg-icons/action/grade';
-import ContentInbox from 'material-ui/svg-icons/content/inbox';
-import ContentDrafts from 'material-ui/svg-icons/content/drafts';
-import ContentSend from 'material-ui/svg-icons/content/send';
+import { List, ListItem } from 'material-ui/List';
 import Subheader from 'material-ui/Subheader';
 import Avatar from 'material-ui/Avatar';
 import FileFolder from 'material-ui/svg-icons/file/folder';
 import {
 blue300,
 indigo900,
-orange200,
-deepOrange300,
-pink400,
-purple500,
 } from 'material-ui/styles/colors';
 
 
-const SideBar = ({folders}) => {
-  console.log(folders);
+const SideBar = (props) => {
+  const handleImageClick = (src) => {
+    props.updateCanvas(src);
+  }
   return (
     <List className="box">
       <Subheader>Uploaded photos</Subheader>
-      {folders.map((folder) => (
+      {props.folders.map((folder) => (
         <ListItem
           primaryText={Object.keys(folder)[0]}
           leftAvatar={<Avatar
             color={blue300}
             backgroundColor={indigo900}
-            icon={<FileFolder />} />}
-          key={folders.indexOf(folder)}
+            icon={<FileFolder />}
+          />}
+          key={props.folders.indexOf(folder)}
           nestedItems={
             folder[Object.keys(folder)[0]].map((image) => (
               <ListItem
                 key={folder[Object.keys(folder)[0]].indexOf(image)}
                 leftAvatar={<Avatar src={image.original_image} size={50}/>}
-                primaryText={image.image_name}
+                primaryText={<div src={image.original_image}>{image.image_name}</div>}
+                name="{image.image_name}"
+                src={image.original_image}
+                onTouchTap={handleImageClick.bind(null, image.original_image)}
               />
             ))
           }
         />
       ))}
     </List>
-  )
+  );
 };
 
 SideBar.PropTypes = {
-  folders: React.PropTypes.array
-}
+  folders: React.PropTypes.array,
+  updateCanvas: React.PropTypes.function,
+};
 
 export default SideBar;
