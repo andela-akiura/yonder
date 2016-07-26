ad50f097b4866e24cf1ece49628fe83eba979be4
Alex Kiura <alex.kiura@andela.com>
2016-07-26 07:57:22 +0300

    [Chore 126484659] Resize canvas.

--- static/js/components/home.jsx
+++ static/js/components/home.jsx
@@ -6,21 +6,16 @@ import SideBar from './sideBar.jsx';
 import request from 'superagent';
 import LinearProgress from 'material-ui/LinearProgress';
 import { Card, CardMedia } from 'material-ui/Card';
-import {GridList, GridTile} from 'material-ui/GridList';
-import {Table,
-  TableBody,
-  TableHeader,
-  TableHeaderColumn,
-  TableRow,
-  TableRowColumn} from 'material-ui/Table';
+import { GridList, GridTile } from 'material-ui/GridList';
 
 
 const style = {
-  home: {
-    // border: 'solid',
-  },
   container: {
     overflow: 'hidden',
+  },
+  canvas: {
+    height: '50%',
+    // maxHeight: '350px',
   },
   gridList: {
     width: '1700px',
@@ -31,9 +26,10 @@ const style = {
     padding: 5,
   },
   gridTile: {
-    width: '200px',
-  }
+    width: '160px',
+  },
 };
+
 const generateFolders = (imageList) => (
   // return a list of unique folders
   Array.from(new Set(imageList.map((image) => {
@@ -91,8 +87,7 @@ class Home extends Component {
         this.setState({ folders });
       });
       fetchImages('/api/v1/thumbnails/').then((response) => {
-        const thumbnails = response[0]['filters'];
-        console.log(thumbnails);
+        const thumbnails = response[0].filters;
         this.setState({ thumbnails });
       });
     } else {
@@ -117,33 +112,39 @@ class Home extends Component {
             <div className="row start-xs">
               <div className="col-xs-3">
                 <SideBar
-                  style={style.home}
                   folders={this.state.folders}
                   updateCanvas={this.updateCanvas}
                 />
               </div>
               <div className="col-xs-1"></div>
               <div className="col-xs-7">
-                <Card>
-                  <CardMedia>
-                    <img width={100} src={this.state.activeImage} />
+                <Card style={style.canvas}>
+                  <CardMedia expandable={false}>
+                    <img src={this.state.activeImage} />
                   </CardMedia>
                 </Card>
                   <br/>
               </div>
               <div className="row">
                 <div className="col-xs-3"></div>
-                <GridList className="col-xs-7"  cols={names.length /2} style={style.gridList} >
-                  {thumbnails.map((thumb) => (
-                    <GridTile style={style.GridTile} title={thumb.filter_name} key={names.indexOf(name)}>
+                <GridList className="col-xs-7"
+                  cols={names.length / 2}
+                  style={style.gridList}
+                >
+                  {this.state.thumbnails.map((thumb) => (
+                    <GridTile
+                      style={style.gridTile}
+                      title={thumb.filter_name}
+                      key={this.state.thumbnails.indexOf(thumb)}
+                    >
                     <img
-                    height="128"
-                    width="128"
-                    src="http://nikonrumors.com/wp-content/uploads/2015/03/Nikon-D7200-sample-images.jpg" />
+                      height="128"
+                      width="128"
+                      src={thumb.filtered}
+                    />
                     </GridTile>
                   ))}
                   </GridList>
-                {/*</div>*/}
               </div>
 
             </div>
